10b4dfc08b583db4b45f24e5979c9072df4d2968
Gor Madatyan <gor-madatyan@proton.me>
2026-07-26 00:19:22 +0400

    Add functionality to query posts doing pagination and full text search

--- src/error.rs
+++ src/error.rs
@@ -7,14 +7,24 @@ pub type Result<T> = std::result::Result<T,AppError>;
 #[derive(Error, Debug)]
 pub enum AppError {
     #[error(transparent)]
+    SqlError(#[from] sqlx::error::Error),
+    #[error(transparent)]
+    JsonError(#[from] serde_json::error::Error),
+    #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         match self {
-            AppError::Other(_) => {
-                (StatusCode::INTERNAL_SERVER_ERROR, "failed".to_string()).into_response()
+            AppError::SqlError(e) =>{
+                (StatusCode::INTERNAL_SERVER_ERROR, format!("{}",e)).into_response()
+            }
+            AppError::JsonError(e) =>{
+                (StatusCode::INTERNAL_SERVER_ERROR, format!("{}",e)).into_response()
+            }
+            AppError::Other(e) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, format!("{}",e)).into_response()
             }
         }
     }
--- src/posts.rs
+++ src/posts.rs
@@ -1,31 +1,90 @@
-use anyhow::{anyhow};
+use crate::error::{AppError, Result};
 use axum::Router;
-use axum::extract::{Query, State};
+use axum::extract::{State};
+use axum_extra::extract::{Query};
+use axum::response::{IntoResponse, Response};
 use axum::routing::get;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sqlx::{Pool, Sqlite};
-use crate::error::{AppError,Result};
+
+#[derive(Serialize)]
+struct Post {
+    post_id: i64,
+    title: String,
+    summary: String,
+    upload_date: String,
+    revision_date: String,
+    body: String,
+    likes: i64,
+}
 
 #[derive(Deserialize)]
 struct PostsQuery {
     page_size: u8,
     page: u32,
     search: Option<String>,
+    #[serde(default)]
+    tag:Vec<String>
+}
+
+#[derive(Serialize)]
+struct PostsResponse {
+    posts: Vec<Post>,
+}
+
+impl IntoResponse for PostsResponse {
+    fn into_response(self) -> Response {
+        serde_json::to_string(&self)
+            .map_err(|e| AppError::from(e))
+            .into_response()
+    }
 }
 
 pub fn get_router() -> Router<Pool<Sqlite>> {
-    Router::new().route(
-        "/posts",
-        get(posts),
-    )
+    Router::new().route("/posts", get(posts))
 }
 
-async fn posts(State(pool): State<Pool<Sqlite>>,
-               Query(PostsQuery {
-                         page_size,
-                         page,
-                         search,
-                     }): Query<PostsQuery>) -> Result<()>{
-    Err(anyhow!("iya"))?;
-    Ok(())
+async fn posts(
+    State(pool): State<Pool<Sqlite>>,
+    Query(PostsQuery {
+        page_size,
+        page,
+        search,
+              tag: tags
+    }): Query<PostsQuery>,
+) -> Result<PostsResponse> {
+    println!("{:?}",tags);
+    let posts = if let Some(search) = search {
+        let search = format!("\"{}\"", search.replace("\"", "\"\""));
+        sqlx::query_as!(
+            Post,
+            "
+    SELECT b.post_id,b.title,b.summary,b.upload_date,b.revision_date,b.body,b.likes
+    FROM blog_posts_fts f
+        INNER JOIN blog_posts b ON f.rowid = b.post_id
+    WHERE blog_posts_fts MATCH ?
+    ORDER BY f.rank
+    LIMIT ? OFFSET ?
+",
+            search,
+            page_size,
+            page * page_size as u32
+        )
+        .fetch_all(&pool)
+        .await?
+    } else {
+        sqlx::query_as!(
+            Post,
+            "
+    SELECT post_id,title,summary,upload_date,revision_date,body,likes
+    FROM blog_posts ORDER BY upload_date LIMIT ? OFFSET ?
+",
+            page_size,
+            page * page_size as u32
+        )
+        .fetch_all(&pool)
+        .await?
+    };
+
+    Ok(PostsResponse { posts })
 }
