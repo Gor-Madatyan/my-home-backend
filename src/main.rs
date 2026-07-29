56fc40324ad8e3a87a4f86a259c50fbdb67af93b
Gor Madatyan <gor-madatyan@proton.me>
2026-07-29 22:25:48 +0400

    Disable CORS for development

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod portfolio;
 
 use anyhow::Result;
 use sqlx::sqlite::SqlitePoolOptions;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,10 +15,16 @@ async fn main() -> Result<()> {
         .connect("sqlite:mydb.db")
         .await?;
 
+    let cors_layer = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
     let router = posts::get_router()
         .merge(citations::get_router())
         .merge(portfolio::get_router())
-        .with_state(connection);
+        .with_state(connection)
+        .layer(cors_layer);
 
     let listener = tokio::net::TcpListener::bind("localhost:8080").await?;
     axum::serve(listener, router).await?;
